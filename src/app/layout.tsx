70acd73cc9d5a81cba4c2c02a78fc5b54497c254
Paha777 <159909421+PavelBoyko07@users.noreply.github.com>
2026-07-07 20:13:28 +0300

    Hotfix Google Tag Manager

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -22,7 +22,9 @@ export default function RootLayout({
   return (
     <html lang="en">
       <head>
+        {/* Google Tag Manager */}
         <script
+          id="google-tag-manager"
           dangerouslySetInnerHTML={{
             __html: `
 (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
@@ -33,8 +35,10 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             `,
           }}
         />
+        {/* End Google Tag Manager */}
       </head>
       <body>
+        {/* Google Tag Manager (noscript) */}
         <noscript>
           <iframe
             src="https://www.googletagmanager.com/ns.html?id=GTM-TTBQ2DMN"
@@ -43,6 +47,7 @@ j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
             style={{ display: 'none', visibility: 'hidden' }}
           />
         </noscript>
+        {/* End Google Tag Manager (noscript) */}
         <SmoothScroll />
         <PageTransition>{children}</PageTransition>
       </body>
