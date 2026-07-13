d3a2742f173b4592ebd3cfded069fdce2fe80e1c
Paha777 <159909421+PavelBoyko07@users.noreply.github.com>
2026-07-13 20:15:42 +0300

    Возврат стилей и логики для секции Cta на главной странице

--- next.config.ts
+++ next.config.ts
@@ -8,6 +8,11 @@ const nextConfig: NextConfig = {
 
   webpack(config) {
     config.module.rules.push({
+      test: /\.webm$/i,
+      type: 'asset/resource',
+    });
+
+    config.module.rules.push({
       test: /\.svg$/i,
       resourceQuery: /component/,
       issuer: /\.[jt]sx?$/,
--- src/types/assets.d.ts
+++ src/types/assets.d.ts
@@ -3,6 +3,11 @@ declare module '*.svg' {
   export default src;
 }
 
+declare module '*.webm' {
+  const src: string;
+  export default src;
+}
+
 declare module '*.avif' {
   const src: string;
   export default src;
